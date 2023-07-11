32d4d8d31e6be8c3b57d12acf9bbc322eb6d24e2
thecosss <thecosss@gmail.com>
2023-07-11 22:36:44 +0200

    Templates gallery page and overall styles

--- src/pages/TemplatesDatabase.vue
+++ src/pages/TemplatesDatabase.vue
@@ -1,7 +1,35 @@
 <template>
-    <div class="form-settings">
-        Todo
+    <div>
+        <div class="block-search">
+            <div class="df gap32">
+                <Textfield />
+                <Btn ghost="true" label="Submit template" />
+            </div>
+            <div class="tags df gap8">
+                Try tags:
+                <div class="tag">Accounts Generator</div>
+                <div class="tag">Submitter</div>
+            </div>
+        </div>
+
+        <div class="templates-list">
+            <template-item />
+        </div>
     </div>
 </template>
 <script setup lang="ts">
+    import Textfield from "../components/Textfield.vue"
+    import Btn from "../components/Btn.vue"
+    import TemplateItem from "../components/TemplateItem.vue"
 </script>
+<style lang="less" scoped>
+.templates-list {
+    margin-top: 32px;
+}
+.block-search {
+    .textfield {width: 300%;}
+}
+.tags {
+    font-size: 13px;
+}
+</style>
